6b9d49ee0b6b8c74380e8af85fff6b3afa11ed92
wendy0705 <wendy9007054@gmail.com>
2024-04-07 20:29:39 +0800

    commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script >
+import Employee from './components/Employee.vue';
+
+export default{
+  name: 'App',
+  components:{
+    Employee
+  }
+}
+</script>
+
+<template>
+  <div id = "app">
+      <Employee/>
+  </div>
+</template>
+
+<style>
+#app {
+  background-size: cover;
+  background-position: center;
+  background-attachment: fixed;
+  background-color: aliceblue;
+  position: fixed;
+  top: 0;
+  left: 0; 
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  z-index: 1; 
+}
+#app .table td, #app .table th{
+  color: black; 
+}
+ 
+</style>
---
+++ src/components/Employee.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class = "container">
+        <h1 class="text-center" style="color: black">Employee List</h1>
+        <table class = "table table-striped">
+                <thead>
+                    <th>Employee Id</th>
+                    <th>Employee First Name</th>
+                    <th>Employee Last Name</th>
+                    <th>Employee Email</th>
+                </thead>
+                <tbody>
+                    <tr v-for = "employee in employees" v-bind:key="employee.id">
+                    <td> {{ employee.id }}</td>
+                    <td> {{ employee.firstName }}</td>
+                    <td> {{ employee.lastName }}</td>
+                    <td> {{ employee.email }}</td>
+                    </tr>
+                </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import EmployeeService from '@/services/EmployeeService';
+
+    export default{
+        name: 'Employees',
+        data(){
+            return{
+                employees : []
+            }
+        },
+        methods:{
+            getEmployees(){
+                EmployeeService.getEmployees().then((response)=>{
+                    this.employees = response.data;
+                });
+            }
+
+        },
+        created(){
+            this.getEmployees()
+        }
+    }
+</script>
+
+<style scoped>
+.container {
+  font-family: Arial, sans-serif;
+  font-size: 16px;
+  font-weight: normal;
+  margin-top: 100px;
+}
+.table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+th, td {
+  border: 1px solid black;
+  padding: 8px;
+  text-align: left;
+}
+
+thead th {
+  background-color: #f2f2f2;
+}
+
+</style>
+
+
