cb0e1caef2bfcf6574371cf9d7b266f488e059a3
wendy0705 <wendy9007054@gmail.com>
2024-04-18 00:23:32 +0800

    remove empty file

---
+++ .idea/backend/src/main/java/com/example/wendycode/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.example.wendycode.repository;
+
+import com.example.wendycode.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
+
