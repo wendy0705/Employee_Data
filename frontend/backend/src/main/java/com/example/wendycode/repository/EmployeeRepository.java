713b5571ff0bfd365f2910893542a1f37a710b67
wendy0705 <wendy9007054@gmail.com>
2024-04-18 18:41:05 +0800

    delete useless folder in frontend folder

--- frontend/backend/src/main/java/com/example/wendycode/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.wendycode.repository;
-
-import com.example.wendycode.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long>{
-
-}
-
